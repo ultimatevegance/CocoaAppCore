2632b79a6e72c6bcc41c80035fb2ada550509c33
Justuswoolworth <martinvegance@yahoo.com>
2023-12-06 20:55:02 +0800

    Initial Commit

---
+++ AppCore/AppCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCoreApp.swift
+//  AppCore
+//
+//  Created by Justus Woolworth on 2023/12/6.
+//
+
+import SwiftUI
+
+@main
+struct AppCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppCore
+//
+//  Created by Justus Woolworth on 2023/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
